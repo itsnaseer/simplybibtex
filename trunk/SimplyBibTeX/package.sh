23ba53dcae4e9ceac240f5d994f8507cec67bde9
seichter <seichter@dc6c3e0b-0943-0410-9947-0992e04f531e>
2005-01-18 09:20:17 +0000

    minor update

--- trunk/SimplyBibTeX/package.sh
+++ trunk/SimplyBibTeX/package.sh
@@ -4,6 +4,8 @@ rm -rf SimplyBibTeX
 cvs -d:pserver:seichter@technotecturecvs.ath.cx:/home/seichter/cvshome export -r HEAD SimplyBibTeX
 
 rm -f SimplyBibTeX/*.sh
+rm -f SimplyBibTeX/*.bat
+rm -f SimplyBibTeX/*.ppr
 
 tar cfjv SimplyBibTeX-`date +%d%m%Y`.tar.gz SimplyBibTeX
 zip -r SimplyBibTeX-`date +%d%m%Y` SimplyBibTeX
